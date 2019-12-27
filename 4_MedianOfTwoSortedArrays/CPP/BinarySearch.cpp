7299ac8b51ff0eb68a7ea2abd29aa66e687d865c
JianweiZheng <jianweizheng@pku.edu.cn>
2019-12-27 15:27:18 +0800

    update the CPP code of 4_MedianOfTwoSortedArrays

--- 4_MedianOfTwoSortedArrays/CPP/BinarySearch.cpp
+++ 4_MedianOfTwoSortedArrays/CPP/BinarySearch.cpp
@@ -1,11 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int M = nums1.size();
-        int N = nums2.size();
-        if (M > N) return findMedianSortedArrays(nums2, nums1);
-        int L = M + N;
-        int k = L / 2; //总共左边需要多少个元素
-        
+        int m = nums1.size();
+        int n = nums2.size();
+        if (m > n) return findMedianSortedArrays(nums2, nums1);
+        int L = m + n;
+        int k = (L+1) / 2; //总共左边需要多少个元素
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = k - i;
+            if (i < iMax && nums2[j-1] > nums1[i]){
+                iMin = i + 1; // i is too small
+            }else if (i > iMin && nums1[i-1] > nums2[j]){
+                iMax = i - 1; // is is too big
+            }else {
+                int maxLeft = 0;
+                if (i == 0){
+                    maxLeft = nums2[j-1];
+                }else if(j == 0){
+                    maxLeft = nums1[i-1];
+                }else{
+                    maxLeft = max(nums1[i-1], nums2[j-1]);
+                }
+                
+                if( (m+n) % 2 == 1) {return maxLeft;}
+                
+                int minRight = 0;
+                if (i == m){
+                    minRight = nums2[j];
+                }else if(j == n){
+                    minRight = nums1[i];
+                }else{
+                    minRight = min(nums1[i], nums2[j]);
+                }
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
     }
 };
