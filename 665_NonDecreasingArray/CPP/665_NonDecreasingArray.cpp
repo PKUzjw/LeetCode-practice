3743fa27576bdfe352c9f7d9e185d64fed1113d3
JianweiZheng <jianweizheng@pku.edu.cn>
2021-01-25 00:52:17 +0800

    add the CPP solution to 665 Non decreasing arrary

---
+++ 665_NonDecreasingArray/CPP/665_NonDecreasingArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int size=nums.size();
+        if(size==1){
+            return true;
+        }
+        //一般处理
+        int count=0;
+        for(int i=1;i<size;i++){
+            if(nums[i-1]>nums[i]){ //头尾元素对以及有药可救的情况
+                count++; //记录逆序数数量
+            }
+            if(count>1){ //判断
+                return false;
+            }
+            if(i>1&&i<size-1&&nums[i+1]<nums[i-1]&&nums[i-2]>nums[i]){ //无力回天
+                return false;
+            }
+        }
+        return true;
+    }
+};
